48109ae5b348bc5b69b7d47c0eee1a98c47189eb
sasokumar692 <shivani.asokumar@gmail.com>
2020-07-19 13:36:24 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Shivani Asokumar on 7/19/20.
+//  Copyright © 2020 Shivani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
